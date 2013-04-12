a5f32a68276bf1b91238bc938184a855c7ac03b1
Elliot Nash <Elliot@venturemedias.com>
2013-04-12 02:12:41 -0400

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venturemedias::Application.config.secret_token = 'a3aa59579277d23e9ba37eb9b0539aa0c3dae00fa764572d73f943e9502ff58c24d621ada591d318342978e6addc4fdabbe91d62ab8d7f0510fbfa4ce9c3bac8'
